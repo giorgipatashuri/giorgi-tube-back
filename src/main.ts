3e3244ca6d86b4bac30907acb5f200460f2c3f82
unknown <patashuri2003@gmail.com>
2023-07-13 20:07:05 +0400

    fixed validation problems

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 import { PrismaService } from './prisma.service'
+import { ValidationPipe } from '@nestjs/common'
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule)
 	const prismaService = app.get(PrismaService)
 	await prismaService.enableShutdownHooks(app)
+	app.useGlobalPipes(new ValidationPipe())
 	app.setGlobalPrefix('api')
 	await app.listen(3000)
 }
