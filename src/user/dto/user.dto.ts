9a0a5e7a16905d573d07a0202a1f38e92f7d4b50
giorgipatashuri <patashuri2003@gmail.com>
2023-07-26 01:14:43 +0400

    updated dto

--- src/user/dto/user.dto.ts
+++ src/user/dto/user.dto.ts
@@ -1,6 +1,6 @@
-import { IsEmail, IsString, MinLength } from 'class-validator'
+import { IsEmail, IsString } from 'class-validator'
 
-export class CreateUserDto {
+export class UserDto {
 	@IsEmail()
 	email: string
 
